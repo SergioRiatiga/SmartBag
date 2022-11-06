ce7ca357ee2176ae04464531e4c7db50fb554479
Sergio <sergioriatiga@gmail.com>
2022-11-05 19:45:42 -0500

    actualizado

---
+++ app/src/main/java/com/misiontic2022/smartbag/views/ui/activities/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.misiontic2022.smartbag.views.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.misiontic2022.smartbag.R
+
+class HomeActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
--- app/src/main/java/com/misiontic2022/smartbag/views/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/misiontic2022/smartbag/views/ui/activities/LoginActivity.kt
@@ -1,14 +1,28 @@
 package com.misiontic2022.smartbag.views.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.misiontic2022.smartbag.R
 import com.misiontic2022.smartbag.databinding.ActivityMainBinding
 
 class LoginActivity: AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var Home: Button
+    lateinit var Registrarse: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        //binding= ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(binding.root)
+        setContentView(R.layout.activity_login)
+        Home= findViewById(R.id.ingresar)
+        Registrarse= findViewById(R.id.registrarse)
+        Home.setOnClickListener {
+            startActivity(Intent(this,HomeActivity::class.java))
+        }
+        Registrarse.setOnClickListener {
+            startActivity(Intent(this,RegistrarseActivity::class.java))
+        }
     }
 }
